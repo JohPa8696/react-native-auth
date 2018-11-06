0c0d941c63ca40490839dc3e38354bf120a88152
JohPa8696 <anynomous@ano.com>
2018-11-06 19:54:28 +1300

    complete authentication form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
-
-
+import LoginForm from './components/LoginForm';
 class App extends Component{
+
+  componentWillMount() {
+      firebase.initializeApp( {
+        apiKey: "AIzaSyD5WFhdTzjRxX9XUrO3uCrLHAcMw4ZeGJQ",
+        authDomain: "authentication-a1564.firebaseapp.com",
+        databaseURL: "https://authentication-a1564.firebaseio.com",
+        projectId: "authentication-a1564",
+        storageBucket: "authentication-a1564.appspot.com",
+        messagingSenderId: "102442862153"
+      }
+    );
+  }
+
   render(){
     return (
       <View>
         <Header headerText="Authentication" />
-          <Text>Hello you!</Text>
+        <LoginForm/>
       </View>
     );
   };
