55bec93809e546897f875fd69aeb08e29c182e6b
JohPa8696 <anynomous@ano.com>
2018-11-06 18:39:11 +1300

    Authentication

---
+++ src/components/common/Card.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { View } from 'react-native';
+const Card = (props) => {
+  const { containerStyle } = styles;
+  return (
+    <View style={containerStyle}>
+     {props.children}
+    </View>
+  );
+};
+
+const styles = {
+  containerStyle: {
+      borderWidth: 1,
+      borderRadius: 2,
+      borderColor: '#ddd',
+      shadowColor: '#000',
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.1,
+      shadowRadius: 2,
+      elavation: 1,
+      marginleft: 5,
+      marginRight: 5,
+      margintop: 15
+  }
+};
+
+export  { Card };
